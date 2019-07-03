2d7af66ce33b1c5ed933f959751b206fd7470e2c
393829394@qq.com <xtlh_jcj_1102>
2019-07-03 15:12:48 +0800

    Initial commit

---
+++ src/main/java/com/jcj/miniweb/MiniwebApplication.java
@@ -0,0 +1,12 @@
+package com.jcj.miniweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniwebApplication.class, args);
+	}
+}
---
+++ src/main/java/com/jcj/miniweb/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @Author： 江成军
+ * @Date：Create on  2019/7/2 21:13
+ * @Description：无
+ */
+package com.jcj.miniweb;
